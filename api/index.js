3719a82cae45db5f9475a16e6ed74baae85eed86
[Saschi] <saschenkagonz@outlook.com>
2022-06-20 14:48:24 -0400

    table force to false

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const app = require("./src/app");
 const { connection } = require("./src/database");
 
 connection
-  .sync({ force: true })
+  .sync({ force: false })
   .then(() => {
     app.listen(3002, () => {
       console.log("Server running on port 3002");
