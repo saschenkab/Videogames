65d94c1673dff7ba8fd5370bfd25237531c6e0ea
[Saschi] <saschenkagonz@outlook.com>
2022-06-20 14:35:10 -0400

    all routers updated

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,12 @@
 const { Router } = require("express");
 const router = Router();
+
 const gamesRouter = require("./videogames");
+const genresRouter = require("./genres");
+const createVideogameRouter = require("./createVideogame");
 
 router.use("/videogames", gamesRouter);
+router.use("/genres", genresRouter);
+router.use("/createVideogame", createVideogameRouter);
 
 module.exports = router;
